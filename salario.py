30610dea6b72a31fe2de3866f935e5180562e431
Robert Rios <84331577+RobertRiosS@users.noreply.github.com>
2024-09-03 17:53:38 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,5 @@
+meusalario = float(input('Informe seu salario:'))
+aumentoSalario = int(input('Iforme a porcentasem do aumento:'))
+oumento = meusalario * aumentoSalario / 100
+novosalario = meusalario + oumento
+print(f'Você deve um aumento no Salario de R${oumento}, seu salario total agora é de R${novosalario}')
